9440178e7df82e14f9bcd3e144b21eb003cc504f
JeeveshN <narang.jeevesh@gmail.com>
2016-11-29 03:38:02 +0530

    Learn From wrong predictions

--- Face-Recog.py
+++ Face-Recog.py
@@ -3,8 +3,8 @@ import sys
 import os
 import re
 import numpy as np
-import shelve
-
+import shelve,random
+from PIL import Image
 if len(sys.argv) < 2:
     print "Usage: python Detect_face.py 'image path'"
     sys.exit()
@@ -13,23 +13,36 @@ image_path=sys.argv[1]
 font = cv2.FONT_HERSHEY_SIMPLEX
 cascade="Face_cascade.xml"
 face_cascade=cv2.CascadeClassifier(cascade)
-d = {0:"Sherlock",1:"Watson"}
+Datafile = shelve.open("Data")
+if 'Data' not in Datafile.keys():
+    Datafile['Data']=list()
+    Data_list = list()
+else:
+    Data_list = Datafile["Data"]
+
+print Datafile["Data"]
+
+def Make_Changes(label):
+    if label not in Data_list:
+        Data_list.append(label)
 
 def get_images(path):
     images = list()
     labels = list()
+    count=0
     for img in os.listdir(path):
-        labl = re.findall("(.*)[0-9]",img)
+        regex = re.compile(r'(\d+|\s+)')
+        labl = regex.split(img)
+        labl = labl[0]
         print labl
-        if "Watson" in labl[0]:
-            c=1
-        else:
-            c=0
+        print count
+        count=count+1
+        Make_Changes(labl)
         image_path =os.path.join(path,img)
         image=cv2.imread(image_path)
         image_grey=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
         images.append(image_grey)
-        labels.append([c])
+        labels.append(Data_list.index(labl))
     return images,labels
 
 #def add_to_dataset(image):
@@ -37,8 +50,21 @@ def initialize_recognizer():
     face_recognizer = cv2.createLBPHFaceRecognizer()
     Dataset = get_images("./Dataset")
     face_recognizer.train(Dataset[0],np.array(Dataset[1]))
+    return face_recognizer
 
-def recognize(image_path):
+def save_wrong_faces(num,temp_set):
+    print "Enter number below face : Correct Name:\n"
+    for i in xrange(num):
+        inp = raw_input()
+        inp = inp.split(":")
+        print inp
+        os.chdir("./Dataset")
+        cv2.imwrite(inp[1]+ str(random.uniform(0,100000))+ ".jpg",temp_set[int(inp[0])])
+        os.chdir("../")
+
+
+
+def recognize(image_path,face_recognizer):
     image=cv2.imread(image_path)
     image_grey=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(image_grey,scaleFactor=1.16,minNeighbors=5,minSize=(25,25),flags=0)
@@ -48,11 +74,26 @@ def recognize(image_path):
         sub_img=image_grey[y-10:y+h+10,x-10:x+w+10]
         temp_set.append(sub_img)
         nbr,conf = face_recognizer.predict(sub_img)
-        print conf
+        print conf,nbr
         cv2.rectangle(image,(x,y),(x+w,y+h),(255, 255,0),2)
-        cv2.putText(image,d[nbr],(x,y-5), font, 0.4,(255,255,0),1)
+        cv2.putText(image,Data_list[nbr],(x,y-5), font, 0.5,(255,255,0),1)
         cv2.putText(image,str(num),(x,y+h+13), font, 0.5,(255,255,0),1)
+        cv2.imwrite("Detected.jpg",image)
         num = int(num)+1
+    Datafile["Data"]=Data_list
+    Datafile.close()
+    os.system("xdg-open Detected.jpg")
+    print "No. of faces predicted wrong:"
+    num_wrong = input()
+    if num_wrong !=0:
+        save_wrong_faces(num_wrong,temp_set)
 
-    cv2.imshow("Faces Found",image)
-    cv2.waitKey(0)
+def main():
+    face_r = initialize_recognizer()
+    recognize(image_path,face_r)
+
+
+
+
+if __name__ == "__main__":
+    main()
