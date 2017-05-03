7196333f6c7ac0a9e01a00081dc2de2e43c0bb13
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-05-04 03:00:20 +0530

    Added directory functionality

---
+++ Trainer/create_dataset.py
@@ -0,0 +1,49 @@
+import cv2
+import random
+import os
+import sys
+
+count = 0
+
+cap = cv2.VideoCapture(0)
+
+
+def start(directory_name):
+	while True:
+		if cap.grab():
+			RET, IMAGE = cap.retrieve()
+			if not RET:
+				continue
+			global count
+			count += 1
+			if count%25 == 0:
+				cv2.imwrite(str(random.uniform(0, 100000)) + ".jpg", IMAGE)
+			cv2.imshow("Video", IMAGE)
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+		count =- 1
+	cap.release()
+	cv2.destroyAllWindows()
+
+
+def main():
+    if len(sys.argv) != 2:
+        print "Usage: create_dataset.py <Name of the person>"
+        sys.exit()
+
+    name = sys.argv[1]
+    name = "./"+name
+
+    if os.path.exists(name):
+        print "name already exists"
+        name = name+str(random.randint(0, 10000))
+        print "So, this name's dataset has been saved as" + name
+    
+    os.makedirs(name)
+    os.chdir(name)
+
+    start(name)
+
+
+if __name__ == "__main__":
+	main()
