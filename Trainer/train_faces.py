78a6325e7d8c3d11c12ea8d69761e38ee2dccb9d
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-05-04 07:03:10 +0530

    Handled invalid directory path

--- Trainer/train_faces.py
+++ Trainer/train_faces.py
@@ -10,7 +10,7 @@ dir_path = False
 def detect(image_path,name):
     image = cv2.imread(os.path.abspath(image_path))
     # cv2.imshow("Faces Found",image)
-    image_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    imagte_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(image_grey,scaleFactor=1.16,minNeighbors=5,minSize=(25,25),flags=0)
     for x,y,w,h in faces:
         sub_img=image[y:y+h,x:x+w]
@@ -37,11 +37,20 @@ def main():
     dir_path = True
     directory_path = directory_path+sys.argv[1]+"/"
 
+    if not os.path.exists(directory_path):
+        print "No images exist for the given person"
+        sys.exit()
+
     os.chdir(directory_path)
+    
     print "Creating Proper Dataset......."
+    images_exist = False
     for img in os.listdir("."):
         if img.endswith('.jpg') or img.endswith('.png') or img.endswith('.jpeg'):
             detect(img, name)
+            images_exist = True
+    if not images_exist:
+        print "No images found to create a dataset"
 
 if __name__ == "__main__":
     main()
